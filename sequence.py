2112692afa120515c35ba2a628d5072f05054f1b
Hinrik Snær <hinni97@hotmail.com>
2019-09-05 11:07:48 +0000

    Added comments to clarify code

--- sequence.py
+++ sequence.py
@@ -7,13 +7,13 @@ number3 = 3
 current_number = 0
 
 while counter_int <= n:
-    if counter_int == 1:
+    if counter_int == 1: # If number entered is one of the first 3 numbers in the sequence, print them out
         print (number1)
     elif counter_int == 2:
         print (number2)
     elif counter_int == 3:
         print (number3)
-    else:
+    else: #If number entered is higher than the initial 3, run algorithm to find each new number in the sequence
         current_number = number1 + number2 + number3
         print (current_number)
         number1 = number2
