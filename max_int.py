b03a670edf092f01c80130d2e7fc4b3a94ae6ae6
Hinrik Snær <hinni97@hotmail.com>
2019-09-05 10:05:26 +0000

    Algorithm written for max_int
    	new file:   max_int.py

---
+++ max_int.py
@@ -0,0 +1,6 @@
+#User inputs int
+#Set int as highest_int
+#Compare each new int to highest_int variable
+#If new int is higher than highest_int, replace value with new int
+#repeat until negative number is entered
+
